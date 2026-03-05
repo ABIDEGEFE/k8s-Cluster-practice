b5667dc3d5bd35ae03764b3e37fc63caabc1a735
Abinet <agonaferdegefe@gmail.com>
2026-03-05 17:07:56 +0300

    remove the logic of baking .env to the docker image and makeing endpoints compatible

--- 3-tier-voteApp/VoteApp/VoteApp/settings.py
+++ 3-tier-voteApp/VoteApp/VoteApp/settings.py
@@ -10,13 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
-from pathlib import Path
-from dotenv import load_dotenv
+#from pathlib import Path
+#from dotenv import load_dotenv
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()
+#load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
